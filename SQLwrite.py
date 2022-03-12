8d9b48cf03a4b05ec5b947bb0a40a610f6f6c46f
jrnaranj <60708726+jrnaranj@users.noreply.github.com>
2022-03-11 22:59:15 -0500

    Added ratings column to database #5

--- SQLwrite.py
+++ SQLwrite.py
@@ -9,12 +9,6 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine('mysql+pymysql://jrnaranj:50309202@oceanus.cse.buffalo.edu/cse442_2022_spring_team_b_db', echo = True)
 engine.connect()
 
-#print(engine) #Verifies that the connection was successful.
-
-#print(engine.table_names())  #Gets the table names
-
-#print(spotify.audio_features[0])
-
 meta = MetaData()
 
 song_data = Table(
@@ -23,7 +17,7 @@ song_data = Table(
    Column('song_name', String(255)), 
    Column('artist_name', String(255)),
    Column('genre_list', PickleType),
-   Column('cover_art', String(255)),
+   Column('cover_url', String(255)),
    Column('danceability', Float),
    Column('energy', Float),
    Column('key', Float),
@@ -36,12 +30,14 @@ song_data = Table(
    Column('valence', Float),
    Column('tempo', Float),
    Column('type', String(255)),
+   Column('song_id', String(255)),
    Column('id', String(255)),
    Column('uri', String(255)),
    Column('track_href', String(255)),
    Column('analysis_url', String(255)),
    Column('duration_ms', Float),
-   Column('time_signature', Integer)
+   Column('time_signature', Integer),
+   Column('rating', Float)
 
 )
 
@@ -56,13 +52,66 @@ Session = sessionmaker(bind=engine)
 
 session = Session()
 
-#data_obj = Table(**spotify.audio_features[0])
-audio_features = spotify.audio_features[0]
 
-ins = song_data.insert()
-ins = song_data.insert().values(**audio_features[0])
-conn = engine.connect()
-result = conn.execute(ins)
+
+def populate_database(year, num_songs):
+   song_dict = spotify.get_data(2019, num_songs) #Populates a list of dictionaries containing the song data.
+
+   for i in range(num_songs): #Populates the database with the number of songs
+      ins = song_data.insert()
+      ins = song_data.insert().values(**song_dict[i])
+      conn = engine.connect()
+      result = conn.execute(ins)
+
+# As of now, populate_database will not account for duplicates. If run multiple times there will be columns with the same song in it.
+# Working on a fix now, if you wish to use populate_database, first drop the table ( DROP TABLE song_name) and then rerun the function.
+
+#populate_database(2019,50)
+
+
+
+def get_song_by_name(song_name):
+   s = song_data.select().where(song_data.c.song_name== song_name ) #SELECT * FROM song_data WHERE 'song_name' = song_name
+   conn = engine.connect()
+   result = conn.execute(s)
+   song_list = []
+
+   for rows in result: #Appends all columns in the table that contain the song name into song_list
+      song_list.append(dict(rows))
+
+   if(len(song_list) == 0): #If list is empty, the database does not contain this song.
+      print('This song is not in the database!')
+
+   return song_list
+
+def get_song_by_id(id):
+   s = song_data.select().where(song_data.c.song_id== id ) #SELECT * FROM song_data WHERE 'id' = isong_d
+   conn = engine.connect()
+   result = conn.execute(s)
+   song_list = []
+
+   for rows in result: #Appends all columns in the table that contain the specific id into song_list
+      song_list.append(dict(rows))
+
+   if(len(song_list) == 0): #If list is empty, the database does not contain this song.
+      print('This song is not in the database!')
+
+   return song_list
+
+def get_song_by_artist(artist_name):
+   s = song_data.select().where(song_data.c.artist_name == None ) #SELECT * FROM song_data WHERE 'id' = isong_d
+   conn = engine.connect()
+   result = conn.execute(s)
+   song_list = []
+
+   for rows in result: #Appends all columns in the table that contain the artist into song_list
+      song_list.append(dict(rows))
+
+   if(len(song_list) == 0): #If list is empty, the database does not contain this song.
+      print('This song is not in the database!')
+
+   return song_list
+
 
 
 
--- spotify.py
+++ spotify.py
@@ -1,21 +1,50 @@
 import spotipy 
 from spotipy.oauth2 import SpotifyClientCredentials
 import credentials
+import pickle
 
 client_credentials_manager = SpotifyClientCredentials(client_id=credentials.client_id, client_secret=credentials.secret_id)
 search_data = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-artist_name = []
-track_name = []
-audio_features = []
-track_id = []
-for i in range(0,1):
-    
-    track = search_data.search(q='year:2020',type='track',limit=50,offset=i)
-    for i, t in enumerate(track['tracks']['items']):
-        artist_name.append(t['artists'][0]['name'])
-        track_name.append(t['name'])
-        track_id.append(t['id'])
-        audio_features.append(search_data.audio_features(t['id']))
 
-#print(audio_features)
+# input the year and the desired number of songs from that year
+def get_data(year, number_of_songs):
+    track = search_data.search(q='year:'+str(year),type='track',limit=number_of_songs,market='us')
+    full_list = []
+    for k,i in enumerate(track['tracks']['items']):
+        
+        full_dictionary = {}
+        
+        #print statements for testing purposes
+        #print("Image url: ", i['album']['images'][1]['url'])
+        #print("Artist ID: ", i['album']['artists'][0]['id'])
+        #print("Artist Name: ", i['artists'][0]['name'])
+        #print("Song ID: ", i['id'])
+        #print("Song Name: ", i['name'])
+        #print(search_data.artist(i['album']['artists'][0]['id'])['genres'])
+        
+        # adding song name
+        full_dictionary.update({"song_name":i['name']})
+
+        #adding artist name
+        full_dictionary.update({"artist_name":i['artists'][0]['name']})
+
+        #adding spotify ID
+        full_dictionary.update({"song_id":i['id']})
+
+        #adding author's list of genres
+        g_list = search_data.artist(i['album']['artists'][0]['id'])['genres']
+        pickled = pickle.dumps(g_list)
+        full_dictionary.update({"genre_list":pickled})
+
+        #adding cover art url
+        full_dictionary.update({"cover_url":i['album']['images'][1]['url']})
+
+        #adding audio features
+        features = search_data.audio_features(i['id'])
+        
+        #adding audio_attributes
+        for k,v in features[0].items():
+            full_dictionary.update({k:v})
+        full_list.append(full_dictionary)
+    return full_list

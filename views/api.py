f75034019e9b3d0c5dfa2c5291b5112ce80a9d8f
stevenAdrianCarter <scarter5@buffalo.edu>
2022-03-14 10:23:29 -0400

    streamlined res_get_playlist and added reccomendation dummy function #22

--- views/api.py
+++ views/api.py
@@ -1,17 +1,14 @@
 from pip import main
+from reccomendation.algorithm import rec_algo
 
-valid_features = ["genre_list", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instumentalness", "liveness", "valence", "tempo", "duration_ms", "time_signature"] #temporary
+valid_features = set("genre_list", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instumentalness", "liveness", "valence", "tempo", "duration_ms", "time_signature") #temporary
 
 def res_get_playlist(in_dict):
-    cleaned = {}
-    if in_dict: return []
-    for feature in valid_features:
-        if feature == "genre_list" and feature in in_dict: cleaned[feature] = in_dict[feature] #filter list
-        elif feature in in_dict: cleaned[feature] = in_dict[feature]
-    if not cleaned:
-        return []
+    if not in_dict: return None
+    for feature in in_dict.keys():
+        if feature not in valid_features: return None
     #Get playlist with cleaned dictionary?
-    playlist = (cleaned)
+    playlist = rec_algo(in_dict)
     return playlist
 
 def res_add_rating(rating: int, description: str):

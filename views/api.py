0c73b8c024791b5ffe882070a12036cf7be115a5
stevenAdrianCarter <scarter5@buffalo.edu>
2022-03-14 10:45:23 -0400

    modified res_get_playlist to catch more errors and added a dummy function for filtering genres #22

--- views/api.py
+++ views/api.py
@@ -5,11 +5,22 @@ valid_features = set("genre_list", "danceability", "energy", "key", "loudness", 
 
 def res_get_playlist(in_dict):
     if not in_dict: return None
+    cleaned = {}
     for feature in in_dict.keys():
-        if feature not in valid_features: return None
+        if feature in valid_features: 
+            if feature == "genre_list": 
+                cleaned_genres = in_dict[feature]
+                if cleaned_genres is not list: return None
+                cleaned[feature] = filter(res_filter_genres, cleaned_genres)
+            else: cleaned[feature] = in_dict[feature]
+    if not cleaned: return None
     #Get playlist with cleaned dictionary?
     playlist = rec_algo(in_dict)
     return playlist
+
+def res_filter_genres(genre_string: str):
+    if genre_string is "ska": return True
+    else: return False
 
 def res_add_rating(rating: int, description: str):
     #Enter rating into a database?

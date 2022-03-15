f971c245faa4716e83ba5790e89dd678a08104d2
stevenAdrianCarter <scarter5@buffalo.edu>
2022-03-14 21:03:04 -0400

    added error handeling to res_get_playlist #22

--- views/api.py
+++ views/api.py
@@ -4,18 +4,22 @@ from reccomendation.algorithm import rec_algo
 valid_features = ["genre_list", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instumentalness", "liveness", "valence", "tempo", "duration_ms", "time_signature"] #temporary
 
 def res_get_playlist(in_dict: dict):
-    if not in_dict: return None
+    if type(in_dict) is not dict: raise TypeError("Parameter entered is not a dictionary.")
+    if not in_dict: raise ValueError("Dictionary entered is empty.")
+    print("passed not in_dict")
     cleaned = {}
     for feature in in_dict.keys():
         if feature in valid_features: 
             if feature == "genre_list": 
                 cleaned_genres = in_dict[feature]
-                if cleaned_genres is not list: return None
-                cleaned[feature] = filter(res_filter_genres, cleaned_genres)
+                if type(cleaned_genres) is not list: raise TypeError('Value for "genre_list" is not a list')
+                cleaned_genres = list(filter(res_filter_genres, cleaned_genres))
+                if cleaned_genres: cleaned["genre_list"] = cleaned_genres
             else: cleaned[feature] = in_dict[feature]
-    if not cleaned: return None
+    if not cleaned: raise ValueError("Input dictionary has no valid parameters")
     #Get playlist with cleaned dictionary?
-    playlist = rec_algo(in_dict)
+    playlist = rec_algo(cleaned)
+    print(cleaned)
     return playlist
 
 def res_filter_genres(genre_string: str):

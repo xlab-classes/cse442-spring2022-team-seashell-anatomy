569c2791226896de4743790a312fcdd6accd5267
Jeffrey Naranjo <jrnaranj@buffalo.edu>
2022-04-11 02:41:21 -0400

    Added songs, share page, and share table (bread moment) #43 #44 #47 #50

--- spotify_api/get_songs.py
+++ spotify_api/get_songs.py
@@ -2,7 +2,10 @@ import requests
 from dotenv import load_dotenv, find_dotenv
 import os
 
+#from request_token import regenerate_bearer_token
 
+
+#regenerate_bearer_token()
 load_dotenv(find_dotenv(), override=True)
 
 headers = {
@@ -13,39 +16,54 @@ headers = {
 
 
 def parse_track(track):
-    url = f'https://api.spotify.com/v1/audio-features/{track["id"]}'
-    print(f'GET {url}')
-    features = requests.get(url, headers=headers)
+    features_url = f'https://api.spotify.com/v1/audio-features/{track["id"]}'
+    artists_url = track['artists'][0]['href']
+    features = requests.get(features_url, headers=headers)
+    artist = requests.get(artists_url, headers=headers)
+
     if features.status_code != 200:
         print(features.text)
         raise ValueError
 
+    if artist.status_code != 200:
+        print(artist.text)
+        raise ValueError
+
+
+    print(f'GET {track["name"]}')
+
     return {
         'song_name': track['name'],
-        'artist_names': [x['name'] for x in track['artists']],
+        'artist_name': track['artists'][0]['name'],
         'song_id': track['id'],
-        'song_features': features.json()
+        'song_features': features.json(),
+        'cover_url': track['album']['images'][1]['url'],
+        'genre_list': artist.json()['genres']
     }
 
 
 def get_songs_by_year(year, limit=None):
     url = f'https://api.spotify.com/v1/search?q=year%3D{year}&type=track'
-    if limit:
-        url += f'&limit={limit}'
+    url += '&limit=10'
 
-    print(f'GET {url}')
     response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         raise ValueError
 
     items = []
-    while response.status_code == 200 and len(items) < limit:
+    while response.status_code == 200:
         json_response = response.json()
         tracks = json_response['tracks']
         next = tracks['next']
-        items += [parse_track(_) for _ in tracks['items']]
-        print(f'GET {next}')
+        for track in tracks['items']:
+            if len(items) == limit:
+                return items
+            try:
+                parsed = parse_track(track)
+                items.append(parsed)
+            except ValueError:
+                print(f'ERROR {track["name"]}')
         response = requests.get(next, headers=headers)
 
     return items
@@ -56,7 +74,8 @@ def get_songs(n):
     retval = []
     
     while n > 0:
-        songs = get_songs_by_year(yr, min(n, 10))
+        print(yr)
+        songs = get_songs_by_year(yr, n)
         retval += songs
         yr -= 1
         n -= len(songs)
@@ -66,4 +85,4 @@ def get_songs(n):
 
 
 if __name__ == '__main__':
-    get_songs(10000)
+    get_songs(5)
